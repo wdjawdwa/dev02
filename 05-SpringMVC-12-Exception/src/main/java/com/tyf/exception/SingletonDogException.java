d6536a49da730c34655c23ab104b158a2f3d8deb
tyf <2292365051@qq.com>
2022-05-29 17:49:22 +0800

    SpringMvc 异常处理

---
+++ 05-SpringMVC-12-Exception/src/main/java/com/tyf/exception/SingletonDogException.java
@@ -0,0 +1,12 @@
+package com.tyf.exception;
+
+// 自定义单身狗异常
+public class SingletonDogException extends Exception {
+    public SingletonDogException() {
+        super();
+    }
+
+    public SingletonDogException(String message) {
+        super(message);
+    }
+}
